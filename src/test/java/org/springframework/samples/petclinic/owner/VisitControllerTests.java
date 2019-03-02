3abe1035104c56950ef7e93376376061715578bc
Calbers4 <b_calvin@hotmail.com>
2019-03-01 21:47:30 -0500

    Testing for Visits being null

--- src/test/java/org/springframework/samples/petclinic/owner/VisitControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/VisitControllerTests.java
@@ -62,6 +62,7 @@ public class VisitControllerTests {
             .andExpect(view().name("redirect:/owners/{ownerId}"));
     }
 
+    
     @Test
     public void testProcessNewVisitFormHasErrors() throws Exception {
         mockMvc.perform(post("/owners/*/pets/{petId}/visits/new", TEST_PET_ID)
@@ -70,6 +71,27 @@ public class VisitControllerTests {
             .andExpect(model().attributeHasErrors("visit"))
             .andExpect(status().isOk())
             .andExpect(view().name("pets/createOrUpdateVisitForm"));
+    }
+   
+
+    @Test
+    public void testProcessNewVisitFormSuccessVisitNull() throws Exception {
+    	
+        Pet testPet = new Pet();
+        
+        testPet.setVisitsInternal(null);
+        testPet.setName("George");
+        testPet.setId(TEST_PET_ID);
+
+        
+        given(this.pets.findById(TEST_PET_ID)).willReturn(testPet);
+        
+        mockMvc.perform(post("/owners/*/pets/{petId}/visits/new", TEST_PET_ID)
+    	    .param("name", "George")
+    	    .param("description", "Visit Description")
+    	)
+	        .andExpect(status().is3xxRedirection())
+	        .andExpect(view().name("redirect:/owners/{ownerId}"));
     }
 
 }
