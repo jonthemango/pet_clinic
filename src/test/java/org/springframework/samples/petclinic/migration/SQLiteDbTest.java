5989026ff10349b32ac975a3b4ef8179d9094daa
jonthemango <jonathan.mongeau@mail.concordia.ca>
2019-03-24 23:02:36 -0400

    Added SQLite Tests

--- src/test/java/org/springframework/samples/petclinic/migration/SQLiteDbTest.java
+++ src/test/java/org/springframework/samples/petclinic/migration/SQLiteDbTest.java
@@ -1,7 +1,11 @@
 package org.springframework.samples.petclinic.migration;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.samples.petclinic.toggles.FeatureToggleManager;
+
+import java.sql.ResultSet;
 
 public class SQLiteDbTest {
 
@@ -10,4 +14,37 @@ public class SQLiteDbTest {
     public void setup() {
         db = new SQLiteDB("db_testing.db");
     }
+
+    @After
+    public void afterTest(){
+        db.close();
+    }
+
+    @Test
+    public void executeQueryTest(){
+        try{
+            db.execute("SELECT * FROM PETS");
+        } catch (Exception e){
+            assert(true);
+        }
+
+
+        try{
+            db.execute("CREATE TABLE IF NOT EXISTS `vets` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `first_name` TEXT, `last_name` TEXT )");
+            assert(true);
+            db.execute("INSERT INTO vets (first_name, last_name) VALUES ('jon', 'mon')");
+            assert(true);
+            ResultSet resultSet = db.select("SELECT * FROM vets WHERE first_name = 'jon'");
+            assert(true);
+            while(resultSet.next()){
+                assert(resultSet.getString("last_name").equals("mon"));
+            }
+        } catch (Exception e){
+            assert(false);
+        }
+
+
+    }
+
+
 }
