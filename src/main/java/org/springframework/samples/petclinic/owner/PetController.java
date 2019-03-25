33d92b77a8a52578df60c68a38ba1c59bc759a6a
Calbers4 <b_calvin@hotmail.com>
2019-03-25 00:52:23 -0400

    petController shadow read

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -25,7 +25,9 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.sql.ResultSet;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * @author Juergen Hoeller
@@ -39,22 +41,26 @@ public class PetController {
     private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
     private final PetRepository pets;
     private final OwnerRepository owners;
-    private SqlDB db;   
+    private SqlDB db;
     private TableDataGateway tdg;
 
     public PetController(PetRepository pets, OwnerRepository owners) {
         this.pets = pets;
         this.owners = owners;
     }
-    
+
     public void setDbForTest(SqlDB db, TableDataGateway tdg) {
         this.db = db;
         this.tdg = tdg;
     }
-    
+
 
     @ModelAttribute("types")
     public Collection<PetType> populatePetTypes() {
+        db = new SQLiteDB();
+        tdg = new TableDataGateway(db);
+        ResultSet resultSet = this.tdg.selectTable("types");
+        shadowReadPetTypes(resultSet);
         return this.pets.findPetTypes();
     }
 
@@ -92,19 +98,19 @@ public class PetController {
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
             // insert in old db
-            this.pets.save(pet); 
+            this.pets.save(pet);
 
             // check if feature toggle is on
-            if(FeatureToggleManager.DO_RUN_CONSISTENCY_CHECKER) 
-            {	
-            	if(!FeatureToggleManager.DOING_MIGRATION_TEST){      
+            if(FeatureToggleManager.DO_RUN_CONSISTENCY_CHECKER)
+            {
+            	if(!FeatureToggleManager.DOING_MIGRATION_TEST){
                     db = new SQLiteDB();
                     tdg = new TableDataGateway(db);
                 }
-            
+
             	// insert into new SQLite db
                 tdg.insertPet(pet);
-            
+
             }
             return "redirect:/owners/{ownerId}";
         }
@@ -113,7 +119,20 @@ public class PetController {
     @GetMapping("/pets/{petId}/edit")
     public String initUpdateForm(@PathVariable("petId") int petId, ModelMap model) {
         Pet pet = this.pets.findById(petId);
+        db = new SQLiteDB();
+        tdg = new TableDataGateway(db);
+        ResultSet resultSet = this.tdg.getById(petId, "pets");
         model.put("pet", pet);
+        try {
+            String name = resultSet.getString("name");
+            String birthDate = resultSet.getString("birth_date");
+            String typeId = tdg.getPetType(resultSet.getInt("type_id"));
+            Integer ownerId = resultSet.getInt("owner_id");
+
+            checkAndUpdate(pet, petId, name, birthDate, typeId, ownerId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
     }
 
@@ -130,4 +149,41 @@ public class PetController {
         }
     }
 
+    private void shadowReadPetTypes(ResultSet resultSet){
+        Collection<PetType> result = this.pets.findPetTypes();
+        Iterator<PetType> oldIterator = result.iterator();
+        try {
+            System.out.println("going into try");
+            while(resultSet.next() && oldIterator.hasNext()){
+                String name = resultSet.getString("name");
+                Integer id = resultSet.getInt("id");
+                PetType nextPetType = oldIterator.next();
+                if (!nextPetType.getName().equals(name)) {
+                    this.tdg.updateInconsistencies(id, "types", "name", nextPetType.getName());
+                }
+                if (!nextPetType.getId().equals(id)) {
+                    this.tdg.updateInconsistencies(id, "types", "id", nextPetType.getId());
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("going into exception here sadly");
+            e.printStackTrace();
+        }
+    }
+
+    public void checkAndUpdate(Pet pet, Integer petId, String name, String birthDate, String typeId, Integer ownerId) {
+        if (!pet.getName().equals(name)) {
+            this.tdg.updateInconsistencies(petId, "pets", "name", pet.getName());
+        }
+        if (!pet.getBirthDate().toString().equals(birthDate)) {
+            this.tdg.updateInconsistencies(petId, "pets", "birth_date", pet.getBirthDate().toString());
+        }
+        if (!(pet.getType().toString().equals(typeId))) {
+            this.tdg.updateInconsistencies(petId, "pets", "type_id", pet.getType().toString());
+        }
+        if (!pet.getOwner().getId().equals(ownerId)) {
+            this.tdg.updateInconsistencies(petId, "pets", "owner_id", pet.getOwner().getId());
+        }
+    }
 }
